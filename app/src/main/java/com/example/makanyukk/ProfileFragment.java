09f5a562f63b4883fbce763d41685962c6f91715
javinrio112 <javinrio112@gmail.com>
2021-04-07 15:53:35 +0700

    adding effect to bottom nav and create profile fragment

---
+++ app/src/main/java/com/example/makanyukk/ProfileFragment.java
@@ -0,0 +1,41 @@
+package com.example.makanyukk;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProfileFragment extends Fragment {
+
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
